aa6deb1b303bc1eb511853b5a1c88d3f20c053cf
Vlad Marchenko <43247911+mrvladkino27@users.noreply.github.com>
2020-05-23 22:56:41 +0300

    fixed import and export

--- export.py
+++ export.py
@@ -3,7 +3,7 @@ import csv
 import cx_Oracle
  
 username = 'system'
-password = '1234567890'
+password = 'ozotos22'
 database = 'localhost/xe'
 
 connection = cx_Oracle.connect(username, password, database) 
@@ -16,53 +16,139 @@ SELECT
     ,TRIM(CAR_COLOR) as car_color
     ,TRIM(CAR_TRANSMISSION) as car_transmission
     ,TRIM(CAR_FUEL) as car_fuel
-    ,TRIM(CAR_COUNT_CYLINDERS) as car_cylinders
+    ,TRIM(CAR_COUNT_CYLYNDERS) as car_cylinders
     ,TRIM(CAR_YEAR) as car_year
     ,TRIM(CAR_MILEAGE) as car_mileage
     ,TRIM(CAR_CONDITION) as car_condition
     ,TRIM(CAR_DRIVE) as car_drive
 FROM CAR""")
- 
- 
+with open("car"+".csv", "w+") as file:
+	writer = csv.writer(file)
+	writer.writerow(["VIN", "MODEL", "YEAR","FUEL","TYPE","TRANSMISSION","COUNT_CYLINDERS","MILEAGE","DRIVE","COLOR","CONDITION"])
+	for car_vin, car_model, car_type, car_color, car_transmission, car_fuel, car_cylinders, car_year, car_mileage, car_condition, car_drive in cursor_car.fetchall():
+		writer.writerow([car_vin, car_model,car_year, car_fuel, car_type, car_transmission, car_cylinders, car_mileage, car_drive, car_color, car_condition])
+print('Car file was created')
+with open("car_model"+".csv", "w+") as file:
+	cursor_car.execute("""
+        SELECT TRIM(model_manufactured) as model
+        , TRIM(model_manufactured) as company
+        FROM CAR_MODEL""")
+	writer = csv.writer(file)
+	writer.writerow(["MODEL", "MANUFACTURED"])
+	for model, company in cursor_car.fetchall():
+		writer.writerow([model,company])
+print('Car_model file was created')
+with open("Manufacturers"+".csv", "w+") as file:
+    cursor_car.execute("""
+        SELECT TRIM(company_name) as company
+        FROM Manufacturer""")
+    writer = csv.writer(file)
+    writer.writerow(["MANUFACTURED"])
+    for company in cursor_car.fetchall():
+		writer.writerow([company])
+print('Manufacturers file was created')
+with open("Color"+".csv", "w+") as file:
+	cursor_car.execute("""
+		SELECT TRIM(color_name) as color
+        FROM Color""")
+	writer = csv.writer(file)
+	writer.writerow(["Colors"])
+	for color in cursor_car.fetchall():
+		writer.writerow([color])
+print('Color file was created')
+with open("Fuel"+".csv", "w+") as file:
+	cursor_car.execute("""
+        SELECT TRIM(fuel_name) as fuel
+        FROM Fuel""")
+	writer = csv.writer(file)
+	writer.writerow(["Fuels"])
+	for fuel in cursor_car.fetchall():
+		writer.writerow([fuel])
+print('Fuel file was created')
+with open("Car_type"+".csv", "w+") as file:
+	cursor_car.execute("""
+        SELECT TRIM(type_name) as ctype
+        FROM CAR_TYPE""")
+	writer = csv.writer(file)
+	writer.writerow(["Types"])
+	for ctype in cursor_car.fetchall():
+		writer.writerow([ctype])
+print('TYPES file was created')
+with open("Transmission"+".csv", "w+") as file:
+	cursor_car.execute("""
+        SELECT TRIM(trans_name) as trans
+        FROM Transmission""")
+	writer = csv.writer(file)
+	writer.writerow(["Transmission"])
+	for trans in cursor_car.fetchall():
+		writer.writerow([trans])
+print('Transmission file was created')
+with open("CAR_CYLINDERS"+".csv","w+") as file:
+	cursor_car.execute("""
+        SELECT TRIM(count_cylinders) as cil
+        FROM CAR_CYLINDERS""")
+	writer = csv.writer(file)
+	writer.writerow(["CAR_CYLINDERS"])
+	for cil in cursor_car.fetchall():
+		writer.writerow([cil])
+print('CAR_CYLINDERS file was created')
+with open("Drive"+".csv", "w+") as file:
+	cursor_car.execute("""
+        SELECT TRIM(drive_name) as dr
+        FROM Drive""")
+	writer = csv.writer(file)
+	writer.writerow(["Drive"])
+	for dr in cursor_car.fetchall():
+		writer.writerow([dr])
+print('Drive file was created')
+with open("US_STATE"+".csv", "w+") as file:
+	cursor_car.execute("""
+        SELECT TRIM(state_name) as st
+        FROM US_STATE""")
+	writer = csv.writer(file)
+	writer.writerow(["US_STATE"])
+	for st in cursor_car.fetchall():
+		writer.writerow([st])
+print('States file was created')
+with open("region"+".csv", "w+") as file:
+	cursor_car.execute("""
+        SELECT TRIM(region_name) as rg
+        , TRIM(region_state) as st
+        FROM Region""")
+	writer = csv.writer(file)
+	writer.writerow(["REGION", "US_STATE"])
+	for rg, st in cursor_car.fetchall():
+		writer.writerow([rg,st])
+print('Region file was created')
+with open("Lot"+".csv", "w+") as file:
+	cursor_car.execute("""
+        SELECT TRIM(lot_price) as pr
+        , TRIM(lot_vin) as vin
+        , TRIM(lot_region) as rg
+        FROM LOT""")
+	writer = csv.writer(file)
+	writer.writerow(["PRICE","VIN","REGION"])
+	for pr,vin,rg in cursor_car.fetchall():
+		writer.writerow([pr,vin,rg])
+print('LOT file was created')
+    # cursor_lot = connection.cursor()
 
-with open("car_model"+".csv", "w+", newline="") as file:
-    for car_vin,car_model,car_type,car_color,car_transmission,car_fuel,car_cylinders,car_year,car_mileage,car_condition,car_drive in cursor_car.fetchall():
-        writer = csv.writer(file)
+    #     query = """
+    #                 SELECT TRIM(LOT.lot_price) AS price
+    #                      ,TRIM(LOT.lot_lat) AS x
+    #                      ,TRIM(LOT.lot_long) AS y
+    #                      ,TRIM(LOT.lot_state) AS state
+    #                      ,TRIM(US_STATE.state_region) AS region
+    #                 FROM CAR JOIN LOT ON CAR.car_vin = LOT.lot_vin
+    #                      JOIN US_STATE ON LOT.lot_state = US_STATE.state_name
+    #                 WHERE CAR.car_vin = :vin"""
  
-        writer.writerow(["VIN", car_vin])
-        writer.writerow(["MODEL", car_model])
-        writer.writerow(["YEAR", car_year])
-        writer.writerow(["FUEL", car_fuel])
-        writer.writerow(["TYPE", car_type])
-        writer.writerow(["TRANSMISSION", car_transmission])
-        writer.writerow(["COUNT_CYLINDERS", car_cylinders])
-        writer.writerow(["MILEAGE", car_mileage])
-        writer.writerow(["DRIVE", car_drive])
-        writer.writerow(["COLOR", car_color])
-        writer.writerow(["CONDITION", car_condition])
-        cursor_car.execute("""
-            SELECT TRIM(CAR_MODEL.model_manufactured)
-            FROM CAR_MODEL
-            WHERE CAR_MODEL.model_name = :model""", model=car_model)
-        writer.writerow(["MANUFACTURER", cursor_car.fetchone()[0]])
-        cursor_lot = connection.cursor()
- 
-        query = """
-                    SELECT TRIM(LOT.lot_price) AS price
-                         ,TRIM(LOT.lot_lat) AS x
-                         ,TRIM(LOT.lot_long) AS y
-                         ,TRIM(LOT.lot_state) AS state
-                         ,TRIM(US_STATE.state_region) AS region
-                    FROM CAR JOIN LOT ON CAR.car_vin = LOT.lot_vin
-                         JOIN US_STATE ON LOT.lot_state = US_STATE.state_name
-                    WHERE CAR.car_vin = :vin"""
- 
-        cursor_lot.execute(query, vin = car_vin)
-        writer.writerow([])
-        writer.writerow([" Price "," Coordinates ", " State "," Region "])
-        for lot_row in cursor_lot:
-            writer.writerow(lot_row)
-        writer.writerow([])
-        writer.writerow([])
+    #     cursor_lot.execute(query, vin = car_vin)
+    #     writer.writerow([])
+    #     writer.writerow([" Price "," Coordinates ", " State "," Region "])
+    #     for lot_row in cursor_lot:
+    #         writer.writerow(lot_row)
+    #     writer.writerow([])
+    #     writer.writerow([])
  
 connection.close()
--- kaggle_import.py
+++ kaggle_import.py
@@ -2,12 +2,12 @@ import cx_Oracle
 import re
 
 username = 'system'
-password = '1234567890'
+password = 'ozotos22'
 database = 'localhost/xe'
 
 connection = cx_Oracle.connect(username, password, database)
 cursor = connection.cursor()
-table_name = ['LOT', 'CAR', 'TRANSMISSION', 'CAR_MODEL', 'MANUFACTURER', ' CAR_TYPE', 'COLOR', 'COUNT_CYLINDERS', 'DRIVE', 'FUEL',  'US_STATE', 'REGION']
+table_name = ['LOT', 'CAR', 'TRANSMISSION', 'CAR_MODEL', 'MANUFACTURER', ' CAR_TYPE', 'COLOR', 'CAR_CYLINDERS', 'DRIVE', 'FUEL', 'REGION' , 'US_STATE']
 for table in table_name:
 	cursor.execute("DELETE FROM "+table)
 
@@ -25,7 +25,8 @@ cylinders = set([])
 fuels = set([])
 vins = set([])
 i = 0
-with open('vehicles.csv', encoding='utf-8', newline='') as f:
+j = 0
+with open('vehicles.csv') as f:
 	f.readline()
 	for line in f:
 		mess = line.split(',')
@@ -51,35 +52,34 @@ with open('vehicles.csv', encoding='utf-8', newline='') as f:
 		if (i%100) == 0:
 				connection.commit()
 		try:
-			if len(mess[2]) > 0 and mess[2] not in regions:
+			if re.match(r'\w{,3}', mess[-3]) and mess[-3] not in states:
 				if re.match(r'\W\b', mess[2]):
 					mess[2] = mess[2][1:]	
-				query = """INSERT INTO Region (region) VALUES(:region)"""
-				regions.add(mess[2])
-				cursor.execute(query, region= mess[2])
-			if re.match(r'\w{,3}', mess[-3]) and mess[-3] not in states:
+				query = """INSERT INTO US_STATE (state_name) VALUES(:state)"""
 				states.add(mess[-3])
-				cursor.execute("INSERT INTO US_STATE(state_name, state_region) VALUES(:state, :region)", state=mess[-3],region=mess[2])
+				cursor.execute(query, state= mess[-3])
+			if  len(mess[2]) > 0 and mess[2] not in regions:
+				regions.add(mess[2])
+				cursor.execute("INSERT INTO REGION(region_state, region_name) VALUES(:state, :region)", state=mess[-3],region=mess[2])
 			if mess[6] not in manufacturers and len(mess[6])>0 and not re.match(r'\W+', mess[6]) and re.match(r'\D',mess[6]):
 				manufacturers.add(mess[6])
 				cursor.execute("INSERT INTO Manufacturer(company_name) VALUES(:company)", company=mess[6])
-			model_id = tuple([mess[7],mess[6]])
-			if mess[6] in manufacturers and model_id not in models and len(mess[7])>0:
-				models.add(model_id)
+			if mess[6] in manufacturers and mess[7] not in models and len(mess[7])>0:
+				models.add(mess[7])
 				cursor.execute("INSERT INTO Car_Model(model_name, model_manufactured) VALUES(:model, :company)", model=mess[7], company=mess[6])
 			if len(mess[18]) == 0:
 					mess[18] = 'red'
-			if mess[18] not in colors and len(mess[18])>0 and not re.match(r'\W+', mess[18]) and re.match(r'\D',mess[18]):
+			if mess[18] not in colors and len(mess[18])>0 and not re.match(r'\W+', mess[18]) and re.match(r'\D',mess[18]) and mess[18] not in types:
 				colors.add(mess[18])
 				cursor.execute("INSERT INTO Color(color_name) VALUES(:color)", color=mess[18])
 			if len(mess[13]) == 0:
 					mess[13] = 'manual'
 			if mess[13] not in transmissions and len(mess[13])>0 and not re.match(r'\W+', mess[13]) and re.match(r'\D',mess[13]):
 				transmissions.add(mess[13])
-				cursor.execute("INSERT INTO Transmission(transmission) VALUES(:trans)", trans=mess[13])
+				cursor.execute("INSERT INTO Transmission(trans_name) VALUES(:trans)", trans=mess[13])
 			if len(mess[15]) == 0:
 					mess[15] = '2wd'
-			if mess[15] not in drives:
+			if mess[15] not in drives and len(mess[15])<5:
 				drives.add(mess[15])
 				cursor.execute("INSERT INTO Drive(drive_name) VALUES(:dr)", dr=mess[15])
 			if len(mess[17]) == 0:
@@ -95,24 +95,28 @@ with open('vehicles.csv', encoding='utf-8', newline='') as f:
 					mess[9] = 4
 			if mess[9] not in cylinders:
 				cylinders.add(mess[9])
-				cursor.execute("INSERT INTO Count_cylinders(count_cylinders) VALUES(:count)", count=int(mess[9]))
-			if mess[10] not in fuels and len(mess[10]) > 0:
+				cursor.execute("INSERT INTO Car_cylinders(count_cylinders) VALUES(:count)", count=int(mess[9]))
+			if mess[10] not in fuels and len(mess[10]) > 0 and len(mess[10])<=9:
 				fuels.add(mess[10])
 				cursor.execute("INSERT INTO Fuel(fuel_name) VALUES(:gas)", gas=mess[10])
 			if len(mess[11]) == 0:
 				mess[11] = 15000
 			if len(mess[8]) == 0:
 				mess[8] = 'new'
-			if mess[14] not in vins and len(mess[14]) > 0 and len(mess[7]) > 0 and re.match(r'\d+', mess[5]) and len(mess[10]) > 0:
+			if mess[14] not in vins and len(mess[14]) > 5 and len(mess[7]) > 0 and re.match(r'\d+', mess[5]) and len(mess[10]) > 0 and mess[7] in models and mess[15] in drives:
 				vins.add(mess[14])
-				cursor.execute("INSERT INTO Car(car_vin, car_model, car_color, car_fuel, car_type, car_transmission, car_count_cylinders, car_year, car_mileage, car_condition, car_drive) VALUES(:a,:b,:c,:d,:e,:f,:g,:h,:k,:l,:m)", a=mess[14],b=mess[7],c=mess[18],d=mess[10],e=mess[17],f=mess[13],g=int(mess[9]),h=int(mess[5]),k=int(mess[11]),l=mess[8],m=mess[15])
-			lost_id = tuple([mess[4], mess[-2], mess[-1][:-2]])
-			if lost_id not in lots and mess[14] in vins:
-				lots.add(lost_id)
-				cursor.execute("INSERT INTO Lot(lot_price, lot_vin, lot_state, lot_lat, lot_long) VALUES(:a,:b,:c,:d,:e)", a=int(lost_id[0]),b=mess[14],c=mess[-3],d=float(lost_id[1]),e=float(lost_id[2]))
+				cursor.execute("INSERT INTO Car(car_vin, car_model, car_color, car_fuel, car_type, car_transmission, car_count_cylynders, car_year, car_mileage, car_condition, car_drive) VALUES(:a,:b,:c,:d,:e,:f,:g,:h,:k,:l,:m)", a=mess[14],b=mess[7],c=mess[18],d=mess[10],e=mess[17],f=mess[13],g=int(mess[9]),h=int(mess[5]),k=int(mess[11]),l=mess[8],m=mess[15])
+			lots_id = tuple([mess[4], mess[14]])
+			if lots_id not in lots and mess[14] in vins:
+				lots.add(lots_id)
+				cursor.execute("INSERT INTO Lot(lot_price, lot_vin, lot_region) VALUES(:a,:b,:c)", a=int(lots_id[0]),b=mess[14],c=mess[2])
+			else: 
+				j += 1
 		except Exception as e:
-			pass
+			print e
 print('finish adding rows. now lets see what`s in db')
+print('all amount of rows readed - ', i)
+print('passed - ', j)
 
 connection.close()
 
